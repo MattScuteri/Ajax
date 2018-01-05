d4c2d7e2f9c6e965cbcbfca6add2a80398ad2453
MattScuteri <mttscuteri@gmail.com>
2018-01-05 17:37:14 -0500

    ajax beginnings

--- script.js
+++ script.js
@@ -5,8 +5,36 @@ $(document).ready(function() {
 	for (let i = 0; i < movieArray.length; i++) {
 		$('#movieButtons').append('<button id="movie">' + movieArray[i] + '</button>');
 		$('#movie').html(movieArray[i]);
+		$('#movie').attr(movieArray[i]);
 	};
 
-	// const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 2NwdkF65AzHSShbXLEjOeP70wFT8HCQ6";
+	$('button').on("click" function() {
 
+		const movie = $(this).attr(movieArray[i]);
+
+		const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "2NwdkF65AzHSShbXLEjOeP70wFT8HCQ6";
+
+		$.ajax({
+			url: queryURL,
+			method: "GET",
+		});
+
+		.done(function(response) {
+			const results = response.data;
+
+			for (let i = 0; i < results.length; i++) {
+				if (results[i].rating !== "g" && results[i].rating !== "pg") {
+					const gifDiv = $('<div class="item">');
+					const rating = rating[i].rating;
+					const p = $('<p>').text("Rating: " + rating);
+					const movieImage = $('<img>');
+					movieImage.attr("src", results[i].images.fixed_height.url);
+					gifDiv.append(p);
+					gifDiv.append(personImage);
+
+					$('#movieGifs').prepend(gifDiv);
+				};
+			};
+		});
+	});	
 });
