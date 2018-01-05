c039ad4a03b1aaf6834a7185d2a987d3354685df
MattScuteri <mttscuteri@gmail.com>
2018-01-05 00:50:48 -0500

    For loop to dynamically add buttons

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
 	let movieArray = ["Raiders of the Lost Ark", "Superman", "The Dark Knight", "Star Wars", "Back to the Future", "Saving Private Ryan", "Forrest Gump", "My Cousin Vinny", "Guardians of the Galaxy", "Deadpool"];
 
 	for (let i = 0; i < movieArray.length; i++) {
-		$('<button>').append(movieArray[i]);
-	}
+		$('#movieButtons').append('<button id="movie">' + movieArray[i] + '</button>');
+		$('#movie').html(movieArray[i]);
+	};
 
 	// const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 2NwdkF65AzHSShbXLEjOeP70wFT8HCQ6";
 
