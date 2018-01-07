0d3eba9be78f0cdcf3fe84d45aa70911a6a43981
MattScuteri <mttscuteri@gmail.com>
2018-01-07 00:43:21 -0500

    Ajax fixes; trying to find link between movies and ajax functions)

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
 
-	let movieArray = ["Raiders of the Lost Ark", "Superman", "The Dark Knight", "Star Wars", "Back to the Future", "Saving Private Ryan", "Forrest Gump", "My Cousin Vinny", "Guardians of the Galaxy", "Deadpool"];
+	let topics = ["Raiders of the Lost Ark", "Superman", "The Dark Knight", "Star Wars", "Back to the Future", "Saving Private Ryan", "Forrest Gump", "My Cousin Vinny", "Guardians of the Galaxy", "Deadpool"];
 
-	for (let i = 0; i < movieArray.length; i++) {
-		$('#movieButtons').append('<button id="movie">' + movieArray[i] + '</button>');
-		$('#movie').html(movieArray[i]);
-		$('#movie').attr(movieArray[i]);
+	for (let i = 0; i < topics.length; i++) {
+		$('#movieButtons').append('<button id="newMovie">' + topics[i] + '</button>');
+		$('#newMovie').addClass(topics[i]);
 	};
 
-	$('button').on("click" function() {
+	console.log(topics);
 
-		const movie = $(this).attr(movieArray[i]);
+	$('#newMovie').on("click", function() {
 
-		const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "2NwdkF65AzHSShbXLEjOeP70wFT8HCQ6";
+		const movie = $(this).attr('.data-movie');
+
+		const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=2NwdkF65AzHSShbXLEjOeP70wFT8HCQ6";
 
 		$.ajax({
 			url: queryURL,
 			method: "GET",
-		});
+		})
 
 		.done(function(response) {
 			const results = response.data;
@@ -25,12 +26,12 @@ $(document).ready(function() {
 			for (let i = 0; i < results.length; i++) {
 				if (results[i].rating !== "g" && results[i].rating !== "pg") {
 					const gifDiv = $('<div class="item">');
-					const rating = rating[i].rating;
+					const rating = results[i].rating;
 					const p = $('<p>').text("Rating: " + rating);
 					const movieImage = $('<img>');
 					movieImage.attr("src", results[i].images.fixed_height.url);
 					gifDiv.append(p);
-					gifDiv.append(personImage);
+					gifDiv.append(movieImage);
 
 					$('#movieGifs').prepend(gifDiv);
 				};
