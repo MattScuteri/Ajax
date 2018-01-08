3eab9f92f8fb68509565da762fef917481193b59
MattScuteri <mttscuteri@gmail.com>
2018-01-08 17:30:43 -0500

    working on still images patch

--- script.js
+++ script.js
@@ -2,18 +2,36 @@ $(document).ready(function() {
 
 	let topics = ["Raiders of the Lost Ark", "Superman", "The Dark Knight", "Star Wars", "Back to the Future", "Saving Private Ryan", "Forrest Gump", "My Cousin Vinny", "Guardians of the Galaxy", "Deadpool"];
 
-	for (let i = 0; i < topics.length; i++) {
-		$('#movieButtons').append('<button id="newMovie">' + topics[i] + '</button>');
-		$('#newMovie').addClass(topics[i]);
+	function createButtons() {
+		$("#movieButtons").empty();
+
+		for (let i = 0; i < topics.length; i++) {
+			let a = $("<button>");
+			a.addClass("movie");
+			a.attr("data-name", topics[i]);
+			a.text(topics[i]);
+			$('#movieButtons').append(a);
+		};
 	};
+
+	createButtons();
 
 	console.log(topics);
 
-	$('#newMovie').on("click", function() {
+	$('#additional').on("click", function(event) {
+		event.preventDefault();
+		let newMovie = $('#movie-input').val().trim();
+		topics.push(newMovie);
+		createButtons();
+	})
 
-		const movie = $(this).attr('.data-movie');
+	$('.movie').on("click", function() {
 
-		const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=2NwdkF65AzHSShbXLEjOeP70wFT8HCQ6";
+		const movie = $(this).attr('data-name');
+
+		const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=2NwdkF65AzHSShbXLEjOeP70wFT8HCQ6&limit=10";
+
+		$('#movieGifs').empty();
 
 		$.ajax({
 			url: queryURL,
@@ -24,7 +42,7 @@ $(document).ready(function() {
 			const results = response.data;
 
 			for (let i = 0; i < results.length; i++) {
-				if (results[i].rating !== "g" && results[i].rating !== "pg") {
+				if (results[i].rating !== "r") {
 					const gifDiv = $('<div class="item">');
 					const rating = results[i].rating;
 					const p = $('<p>').text("Rating: " + rating);
